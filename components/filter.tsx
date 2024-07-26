44d73bd11545da7e89f1172fb72b3b9c06a493f6
Mohnish <mohnishgopi@gmail.com>
2024-07-26 17:05:27 +0400

    Update filter.tsx

--- components/filter.tsx
+++ components/filter.tsx
@@ -29,12 +29,12 @@ const ExploreTopics = () => {
     <div className="max-w-6xl mx-auto p-6">
       <div className="flex justify-between items-center mb-4">
         <h1 className="text-3xl font-bold">Explore Topics</h1>
-        <a href="/documents/introduction" className=" text-[#196A76] hover:underline">View All</a>
+        <a href="/documents/introduction" className=" text-[#078A9F] hover:underline">View All</a>
       </div>
       {/* Buttons for larger screens */}
       <div className="hidden sm:flex space-x-4 mb-6">
         <button
-          className={`px-4 py-2 font-semibold ${selectedTopic === 'All' ? 'bg-[#F2FBFC] text-[#196A76] rounded-full dark:bg-[#196A76] dark:text-white' : ''}`}
+          className={`px-4 py-2 font-semibold ${selectedTopic === 'All' ? 'bg-[#F2FBFC] text-[#078A9F] rounded-xl dark:bg-[#078A9F] dark:text-white' : ''}`}
           onClick={() => handleTopicClick('All')}
         >
           All
@@ -42,7 +42,7 @@ const ExploreTopics = () => {
         {topics.map((topic) => (
           <button
             key={topic}
-            className={`px-4 py-2 font-semibold ${selectedTopic === topic ? 'bg-[#F2FBFC] text-[#196A76] rounded-full dark:bg-[#196A76] dark:text-white' : ''}`}
+            className={`px-4 py-2 font-semibold ${selectedTopic === topic ? 'bg-[#F2FBFC] text-[#078A9F] rounded-xl dark:bg-[#078A9F] dark:text-white' : ''}`}
             onClick={() => handleTopicClick(topic)}
           >
             {topic}
@@ -52,7 +52,7 @@ const ExploreTopics = () => {
       {/* Dropdown for smaller screens */}
       <div className="sm:hidden mb-6">
         <button
-          className="px-4 py-2 font-semibold bg-[#F2FBFC] text-[#196A76] dark:bg-[#196A76] dark:text-white rounded-full w-full text-left"
+          className="px-4 py-2 font-semibold bg-[#F2FBFC] text-[#078A9F] dark:bg-[#078A9F] dark:text-white rounded-xl w-full text-left"
           onClick={() => setDropdownOpen(!dropdownOpen)}
         >
           {selectedTopic} {dropdownOpen ? '▲' : '▼'}
@@ -60,7 +60,7 @@ const ExploreTopics = () => {
         {dropdownOpen && (
           <div className="mt-2 flex flex-col space-y-2">
             <button
-              className={`px-4 py-2 font-semibold ${selectedTopic === 'All' ? 'bg-[#F2FBFC] text-[#196A76] rounded-full dark:bg-[#196A76] dark:text-white' : ''}`}
+              className={`px-4 py-2 font-semibold ${selectedTopic === 'All' ? 'bg-[#F2FBFC] text-[#078A9F] rounded-xl dark:bg-[#078A9F] dark:text-white' : ''}`}
               onClick={() => handleTopicClick('All')}
             >
               All
@@ -68,7 +68,7 @@ const ExploreTopics = () => {
             {topics.map((topic) => (
               <button
                 key={topic}
-                className={`px-4 py-2 font-semibold ${selectedTopic === topic ? 'bg-[#F2FBFC] text-[#196A76] dark:bg-[#196A76] dark:text-white rounded-full' : ''}`}
+                className={`px-4 py-2 font-semibold ${selectedTopic === topic ? 'bg-[#F2FBFC] text-[#078A9F] dark:bg-[#078A9F] dark:text-white rounded-xl' : ''}`}
                 onClick={() => handleTopicClick(topic)}
               >
                 {topic}
@@ -80,7 +80,7 @@ const ExploreTopics = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {filteredCards.map((card) => (
           <div key={card.id}>
-            <div className="block mt-2 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-[#12363c45] dark:border-[#10292db4] dark:hover:bg-[#196a7640]">
+            <div className="block mt-2 max-w-sm p-6 bg-white border border-gray-200 rounded-xl shadow hover:bg-gray-100 dark:bg-[#12363c45] dark:border-[#10292db4] dark:hover:bg-[#39828d40]">
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{card.topic}</h5>
               <p className="font-normal text-gray-700 dark:text-gray-400">{card.description}</p>
             </div>
