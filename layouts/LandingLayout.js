a0d6d97fef326547a8140539ca01e869cd928f19
Mohnish <mohnishgopi@gmail.com>
2024-08-05 20:58:17 +0530

    test

--- layouts/LandingLayout.js
+++ layouts/LandingLayout.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// Provide a default themeConfig if the import fails
 const defaultThemeConfig = {
   navbar: 'Default Navbar',
   footer: 'Default Footer',
@@ -16,10 +15,8 @@ try {
 const LandingLayout = ({ children }) => {
   return (
     <div className="layout">
-      {/* Render Navbar */}
       <nav>{themeConfig?.navbar}</nav>
       <main>{children}</main>
-      {/* Render Footer */}
       <footer>{themeConfig?.footer}</footer>
     </div>
   );
